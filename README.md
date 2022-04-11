cb0184305d5d2e0dc25c2a359138fc7f054cc113
Malik <malikspruill@gmail.com>
2022-04-11 00:52:09 -0400

    Added questions for README generator using inquirer npm package

--- README.md
+++ README.md
@@ -5,4 +5,6 @@
 ## Installation
 
 ## Contributions
-
+Malik Spruill
+Xandromus
+gchoi2u
--- src/index.js
+++ src/index.js
@@ -6,6 +6,148 @@ const inquirer = require("inquirer");
 const questions = [
     {
         type: "input",
+        name: "github",
+        message: "Enter your github username. *username will be added to questions section of README (Required)",
+        validate: questionInput => {
+            if (questionInput) {
+                return true;
+            }
+            console.log("You have to type in the title of your project!");
+            return false;
+        }
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "Enter your email address. *email will be added to questions section of README (Required)",
+        validate: questionInput => {
+            if (questionInput) {
+                return true;
+            }
+            console.log("You have to type in the title of your project!");
+            return false;
+        }
+    },
+    {
+        type: "input",
+        name: "projectTitle",
+        message: "What is the title of your project? (Required)",
+        validate: questionInput => {
+            if (questionInput) {
+                return true;
+            }
+            console.log("You have to type in the title of your project!");
+            return false;
+        }
+    },
+    {
+        type: "confirm",
+        name: "confirmTableOfContents",
+        message: "Would you like to add an TableOfContents section to your project?",
+        default: false
+    },
+    {
+        type: "confirm",
+        name: "confirmInstallation",
+        message: "Would you like to add an installation section to your project?",
+        default: false
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "Provide some information in the installation section",
+        when: ({confirmInstallation}) => {
+            if (confirmInstallation) {
+                return true;
+            }
+            return false;
+        }
+    },
+    {
+        type: "confirm",
+        name: "confirmUsage",
+        message: "Would you like to add an Usage section to your project?",
+        default: false
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "Provide some information in the Usage section",
+        when: ({confirmUsage}) => {
+            if (confirmUsage) {
+                return true;
+            }
+            return false;
+        }
+    },
+    {
+        type: "confirm",
+        name: "confirmLicense",
+        message: "Would you like to add an License section to your project?",
+        default: false
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "Choose a license you would like to add to your project, or choose 'none' if you've changed your mind.",
+        choices: ["MIT","Apache"],
+        when: ({confirmLicense}) => {
+            if (confirmLicense) {
+                return true;
+            }
+            return false;
+        }
+    },
+    {
+        type: "confirm",
+        name: "confirmContributing",
+        message: "Would you like to add an Contributing section to your project?",
+        default: false
+    },
+    {
+        type: "input",
+        name: "contributing",
+        message: "Provide some information in the Contributing section",
+        when: ({confirmContributing}) => {
+            if (confirmContributing) {
+                return true;
+            }
+            return false;
+        }
+    },
+    {
+        type: "confirm",
+        name: "confirmTests",
+        message: "Would you like to add a Tests section to your project?",
+        default: false
+    },
+    {
+        type: "input",
+        name: "tests",
+        message: "Provide some information in the Tests section",
+        when: ({confirmTests}) => {
+            if (confirmTests) {
+                return true;
+            }
+            return false;
+        }
+    },
+    {
+        type: "confirm",
+        name: "confirmQuestions",
+        message: "Would you like to add a Questions section to your project?",
+        default: false
+    },
+    {
+        type: "input",
+        name: "questions",
+        message: "Provide some information in the Questions section",
+        when: ({confirmQuestions}) => {
+            if (confirmQuestions) {
+                return true;
+            }
+            return false;
+        }
     }
 ];
 
@@ -13,7 +155,9 @@ const questions = [
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+//function init() {}
 
 // Function call to initialize app
-init();
+//init();
+
+inquirer.prompt(questions);
